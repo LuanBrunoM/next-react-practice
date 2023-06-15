74f67c331c2986d585d120d3f96855f6a7277e78
Luan Bruno <luanbrunomelo03@gmail.com>
2023-06-15 15:55:37 -0300

    lists

--- src/constants/produtos.ts
+++ src/constants/produtos.ts
@@ -6,7 +6,7 @@ const produtos: Produto[] = [
     nome: "Camiseta",
     preco: 39.9,
     descricao: "Camiseta branca de algodão",
-    imagem: 'https://source.unsplash.com/featured/300x300?tshirt',
+    imagem: 'https://source.unsplash.com/featured/300x200?tshirt',
 
   },
   {
@@ -14,41 +14,43 @@ const produtos: Produto[] = [
     nome: "Calça Jeans",
     preco: 99.9,
     descricao: "Calça azul de algodão",
-    imagem: 'https://source.unsplash.com/featured/300x300?jeans',
+    imagem: 'https://source.unsplash.com/featured/300x200?jeans',
   },
   {
     id: 3,
     nome: "Tênis",
     preco: 192.9,
     descricao: "Tênis Esportivo",
-    imagem: 'https://source.unsplash.com/featured/300x300?sneakers',
+    imagem: 'https://source.unsplash.com/featured/300x200?sneakers',
   },
   {
     id: 4,
     nome: "Camisa Polo",
     preco: 79.9,
     descricao: "Camisa Polo preta",
-    imagem: 'https://source.unsplash.com/featured/300x300?jeans',
+    imagem: 'https://source.unsplash.com/featured/300x200?poloshirt',
   },
   {
     id: 5,
     nome: "Camisa Social",
     preco: 129.9,
     descricao: "Camisa social cinza",
-    imagem: 'https://source.unsplash.com/featured/300x300?shird',
+    imagem: 'https://source.unsplash.com/featured/300x200?shirt',
   },
   {
     id: 6,
     nome: "Blusa",
     preco: 39.9,
     descricao: "Blusa de lã",
-    imagem: 'https://source.unsplash.com/featured/300x300?blouse',
+    imagem: 'https://source.unsplash.com/featured/300x200?blouse',
   },
   {
     id: 7,
     nome: "Jaqueta",
     preco: 249.9,
     descricao: "Jaqueta de couro",
-    imagem: 'https://source.unsplash.com/featured/300x300?jacket',
+    imagem: 'https://source.unsplash.com/featured/300x200?jacket',
   },
 ]
+
+export default produtos
--- src/pages/listas/basica.tsx
+++ src/pages/listas/basica.tsx
@@ -1,4 +1,4 @@
-import TabelaMSI from "./TabelaMSI"
+import TabelaMSI from "../../components/listas/TabelaMSI"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrophy } from "@fortawesome/free-solid-svg-icons";
 
